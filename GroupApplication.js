8304244a5acba6c0834a166252e99afd4caa6a50
Chase Price <siscopie@gmail.com>
2016-07-28 14:41:24 -0700

    added push/splice

--- GroupApplication.js
+++ GroupApplication.js
@@ -11,5 +11,6 @@ var aQuestion = [new Question("The sky is yellow True or False?", "false", "tf")
     new Question("5+2=___", "7", "blank"),
     new Question("If you cut an earthworm in half, both halves can regrow their body. True or False", "true", "tf"),
     new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int", "b", "mc")];
-var bQuestions = [];
+var bQuestion = [];
 var q1 = Math.floor(Math.random() * aQuestion.length);
+bQuestion.push(aQuestion.splice(q1));
--- GroupApplication.ts
+++ GroupApplication.ts
@@ -7,5 +7,6 @@ new Question("How many teachers and TAs do we have?","5","mc"),
 new Question("5+2=___","7","blank"),
 new Question("If you cut an earthworm in half, both halves can regrow their body. True or False","true","tf"),
 new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int","b","mc")];
-let bQuestions = [];
+let bQuestion = [];
 let q1 = Math.floor(Math.random() * aQuestion.length);
+bQuestion.push(aQuestion.splice(q1));

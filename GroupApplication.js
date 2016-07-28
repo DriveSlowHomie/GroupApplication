93e86df4cedecf92dad3976011cb86edf25c52c2
Shane Burton <burtosha13@gmail.com>
2016-07-28 16:37:03 -0700

    Added input form function

--- GroupApplication.js
+++ GroupApplication.js
@@ -7,7 +7,7 @@ var Question = (function () {
     return Question;
 }());
 var aQuestion = [new Question("The sky is yellow True or False?", "false", "tf"),
-    new Question("How many teachers and TAs do we have?", "5", "mc"),
+    new Question("How many teachers and TAs do we have? a: 1, b: 2, c: 80, d:5 ", "d", "mc"),
     new Question("5+2=___", "7", "blank"),
     new Question("If you cut an earthworm in half, both halves can regrow their body. True or False", "true", "tf"),
     new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int", "b", "mc")];
@@ -23,9 +23,23 @@ for (var i = 0; i < 3; i++) {
 }
 function displayTest() {
     for (var i = 0; i < bQuestion.length; i++) {
-        elementArray[i].innerHTML = bQuestion[i].question;
+        elementArray[i].innerHTML = bQuestion[i].question + typeChecker(bQuestion[i]);
         console.log(bQuestion[i].question);
         console.log(elementArray);
+    }
+}
+function typeChecker(question) {
+    if (question.type === "tf") {
+        var trueFalse = "<select name=\"trueFalse\" form=\"trueform\">\n      <option value=\"true\">true</option>\n      <option value=\"false\">false</option>\n    </select>";
+        return trueFalse;
+    }
+    else if (question.type === "mc") {
+        var mcElement = "<form>\n      <input type=\"radio\" name=\"mc\" value=\"a\" checked> A.<br>\n      <input type=\"radio\" name=\"mc\" value=\"b\"> B.<br>\n      <input type=\"radio\" name=\"mc\" value=\"c\"> C.<br>\n      <input type=\"radio\" name=\"mc\" value=\"d\"> D.<br>\n    </form>";
+        return mcElement;
+    }
+    else if (question.type === "blank") {
+        var blankElement = "<form>\n  Enter your answer:<br>\n  <input type=\"text\" name=\"blank\"><br>\n</form>";
+        return blankElement;
     }
 }
 displayTest();
--- GroupApplication.ts
+++ GroupApplication.ts
@@ -1,20 +1,18 @@
-class Question
-{
-  constructor(public question:string, public answer:string, public type:string){}
+class Question {
+    constructor(public question: string, public answer: string, public type: string) { }
 }
 let aQuestion = [new Question("The sky is yellow True or False?", "false", "tf"),
-new Question("How many teachers and TAs do we have?","5","mc"),
-new Question("5+2=___","7","blank"),
-new Question("If you cut an earthworm in half, both halves can regrow their body. True or False","true","tf"),
-new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int","b","mc")];
+    new Question("How many teachers and TAs do we have? a: 1, b: 2, c: 80, d:5 ", "d", "mc"),
+    new Question("5+2=___", "7", "blank"),
+    new Question("If you cut an earthworm in half, both halves can regrow their body. True or False", "true", "tf"),
+    new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int", "b", "mc")];
 let bQuestion = [];
-let q1  = document.getElementById("q1");
+let q1 = document.getElementById("q1");
 let q2 = document.getElementById("q2");
 let q3 = document.getElementById("q3");
-let elementArray = [q1,q2,q3];
+let elementArray = [q1, q2, q3];
 
-for(let i = 0; i < 3; i++)
-{
+for (let i = 0; i < 3; i++) {
     let question1 = Math.floor(Math.random() * aQuestion.length);
     bQuestion.push(aQuestion[question1]);
     aQuestion.splice(question1, 1);
@@ -23,16 +21,51 @@ for(let i = 0; i < 3; i++)
 }
 
 
-function displayTest()
-{
-  for(let i = 0; i<bQuestion.length; i++)
-  {
-    //debugger;
-    elementArray[i].innerHTML = bQuestion[i].question;
-    console.log(bQuestion[i].question);
-    console.log(elementArray);
+function displayTest() {
+    for (let i = 0; i < bQuestion.length; i++) {
+        elementArray[i].innerHTML = bQuestion[i].question + typeChecker(bQuestion[i]);
 
-  }
+
+        console.log(bQuestion[i].question);
+        console.log(elementArray);
+
+    }
+}
+
+function typeChecker(question) {
+    if (question.type === "tf") {
+
+        let trueFalse = `<select name="trueFalse" form="trueform">
+      <option value="true">true</option>
+      <option value="false">false</option>
+    </select>`;
+
+        return trueFalse;
+
+    }
+    else if (question.type === "mc") {
+
+        let mcElement = `<form>
+      <input type="radio" name="mc" value="a" checked> A.<br>
+      <input type="radio" name="mc" value="b"> B.<br>
+      <input type="radio" name="mc" value="c"> C.<br>
+      <input type="radio" name="mc" value="d"> D.<br>
+    </form>`
+
+        return mcElement;
+
+
+
+    }
+    else if (question.type === "blank") {
+        let blankElement = `<form>
+  Enter your answer:<br>
+  <input type="text" name="blank"><br>
+</form>`
+
+        return blankElement;
+
+    }
 }
 
 

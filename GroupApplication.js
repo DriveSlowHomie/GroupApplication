c6b7eeef234a605348b04d6ee10a1f3285615b41
Chase Price <siscopie@gmail.com>
2016-07-28 14:32:16 -0700

    added questions array

---
+++ GroupApplication.js
@@ -0,0 +1,15 @@
+var Question = (function () {
+    function Question(question, answer, type) {
+        this.question = question;
+        this.answer = answer;
+        this.type = type;
+    }
+    return Question;
+}());
+var aQuestion = [new Question("The sky is yellow True or False?", "false", "tf"),
+    new Question("How many teachers and TAs do we have?", "5", "mc"),
+    new Question("5+2=___", "7", "blank"),
+    new Question("If you cut an earthworm in half, both halves can regrow their body. True or False", "true", "tf"),
+    new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int", "b", "mc")];
+var bQuestions = [];
+var q1 = Math.floor(Math.random() * aQuestion.length);
---
+++ GroupApplication.ts
@@ -0,0 +1,11 @@
+class Question
+{
+  constructor(public question:string, public answer:string, public type:string){}
+}
+let aQuestion = [new Question("The sky is yellow True or False?", "false", "tf"),
+new Question("How many teachers and TAs do we have?","5","mc"),
+new Question("5+2=___","7","blank"),
+new Question("If you cut an earthworm in half, both halves can regrow their body. True or False","true","tf"),
+new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int","b","mc")];
+let bQuestions = [];
+let q1 = Math.floor(Math.random() * aQuestion.length);

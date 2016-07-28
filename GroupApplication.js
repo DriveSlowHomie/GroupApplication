2454c26fc2c86da2811f8d1671356fb7d3f06c97
Emma Giap <ptgiap1991@me.com>
2016-07-28 18:48:00 -0400

    added display function()

--- GroupApplication.js
+++ GroupApplication.js
@@ -12,5 +12,20 @@ var aQuestion = [new Question("The sky is yellow True or False?", "false", "tf")
     new Question("If you cut an earthworm in half, both halves can regrow their body. True or False", "true", "tf"),
     new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int", "b", "mc")];
 var bQuestion = [];
-var q1 = Math.floor(Math.random() * aQuestion.length);
-bQuestion.push(aQuestion.splice(q1));
+var q1 = document.getElementById("q1");
+var q2 = document.getElementById("q2");
+var q3 = document.getElementById("q3");
+var elementArray = [q1, q2, q3];
+for (var i = 0; i < 3; i++) {
+    var question1 = Math.floor(Math.random() * aQuestion.length);
+    bQuestion.push(aQuestion[question1]);
+    aQuestion.splice(question1, 1);
+}
+function displayTest() {
+    for (var i = 0; i < bQuestion.length; i++) {
+        elementArray[i].innerHTML = bQuestion[i].question;
+        console.log(bQuestion[i].question);
+        console.log(elementArray);
+    }
+}
+displayTest();
--- GroupApplication.ts
+++ GroupApplication.ts
@@ -8,5 +8,33 @@ new Question("5+2=___","7","blank"),
 new Question("If you cut an earthworm in half, both halves can regrow their body. True or False","true","tf"),
 new Question("When creating a variable in js use the following syntax: a:var , b:let, c:variable, d:int","b","mc")];
 let bQuestion = [];
-let q1 = Math.floor(Math.random() * aQuestion.length);
-bQuestion.push(aQuestion.splice(q1));
+let q1  = document.getElementById("q1");
+let q2 = document.getElementById("q2");
+let q3 = document.getElementById("q3");
+let elementArray = [q1,q2,q3];
+
+for(let i = 0; i < 3; i++)
+{
+    let question1 = Math.floor(Math.random() * aQuestion.length);
+    bQuestion.push(aQuestion[question1]);
+    aQuestion.splice(question1, 1);
+
+
+}
+
+
+function displayTest()
+{
+  for(let i = 0; i<bQuestion.length; i++)
+  {
+    //debugger;
+    elementArray[i].innerHTML = bQuestion[i].question;
+    console.log(bQuestion[i].question);
+    console.log(elementArray);
+
+  }
+}
+
+
+
+displayTest();
